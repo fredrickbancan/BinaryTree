2d8fbc1f33559f061371769a168e4fb8a3a81863
rabbet35 <rabbet35@hotmail.com>
2020-10-01 16:50:17 +1000

    working towards implimenting all functions

--- BinaryTree/BinaryTree.cpp
+++ BinaryTree/BinaryTree.cpp
@@ -1,20 +1,124 @@
 #include "BinaryTree.h"
 
+bool BinaryTree::searchForValueRecursive(Node* root, int value) const
+{
+	if (root->data == value)
+	{
+		return true;
+	}
+
+	if (value > root->data && root->rightChild != nullptr)
+	{
+		
+		return searchForValueRecursive(root->rightChild, value);
+		
+	}
+	else if (root->leftChild != nullptr)
+	{
+		return searchForValueRecursive(root->leftChild, value);
+	}
+	return false;
+}
+
+bool BinaryTree::searchAndDeleteLeafRecursive(Node* parentLeaf, int value)
+{
+	return false;
+}
+
 BinaryTree::BinaryTree(bool startWithRoot /*= false*/, int rootValue/*= 0*/)
 {
 	if (startWithRoot)
 	{
-		root = new Node{nullptr, nullptr, rootValue};
+		root = new Node{nullptr, nullptr, nullptr, rootValue};
 		count++;
 	}
 }
 
-void BinaryTree::deleteLeafAndBranches(Node* root)
+void BinaryTree::deleteLeafAndBranchesRecursive(Node* leaf)
 {
-	//TODO: impliment
+	if (leaf == nullptr)
+	{
+		return;
+	}
+
+	deleteLeafAndBranchesRecursive(leaf->leftChild);
+	deleteLeafAndBranchesRecursive(leaf->rightChild);
+
+	//changing parents pointer to this node to nullptr will prevent pointing to invalid memory
+	if (leaf->parent != nullptr)
+	{
+		if (leaf->parent->leftChild == leaf)
+		{
+			leaf->parent->leftChild = nullptr;
+		}
+		else if (leaf->parent->rightChild == leaf)
+		{
+			leaf->parent->rightChild = nullptr;
+		}
+	}
+
+	count--;
+	delete leaf;
+}
+
+void BinaryTree::insertNodeRecursive(Node* parentLeaf, Node* newNode)
+{
+	if (newNode->data > parentLeaf->data)
+	{
+		if (parentLeaf->rightChild == nullptr)
+		{
+			newNode->parent = parentLeaf;
+			parentLeaf->rightChild = newNode;
+			isEmpty = false;
+			count++;
+			return;
+		}
+		insertNodeRecursive(parentLeaf->rightChild, newNode);
+	}
+	else
+	{
+		if (parentLeaf->leftChild == nullptr)
+		{
+			newNode->parent = parentLeaf;
+			parentLeaf->leftChild = newNode;
+			isEmpty = false;
+			count++;
+			return;
+		}
+		insertNodeRecursive(parentLeaf->leftChild, newNode);
+	}
 }
 
 BinaryTree::~BinaryTree()
 {
+	deleteLeafAndBranchesRecursive(root);
+}
 
+void BinaryTree::addNode(int value)
+{
+	if (isEmpty)
+	{
+		root = new Node{ nullptr, nullptr, nullptr, value };
+		count++;
+		isEmpty = false;
+		return;
+	}
+	insertNodeRecursive(root, new Node{nullptr, nullptr, nullptr, value});
+}
+
+bool BinaryTree::contains(int value) const
+{
+	if(isEmpty)
+	return false;
+
+	return searchForValueRecursive(root, value);
+}
+
+bool BinaryTree::remove(int value)
+{
+	if (isEmpty)
+		return false;
+
+
+	return searchAndDeleteLeafRecursive(root, value);
 }
--- BinaryTree/BinaryTree.h
+++ BinaryTree/BinaryTree.h
@@ -2,6 +2,7 @@
 
 struct Node
 {
+	Node* parent = nullptr;
 	Node* leftChild = nullptr;
 	Node* rightChild = nullptr;
 	int data = 0;
@@ -16,9 +17,34 @@ private:
 
 	/*recursively removes all leaf nodes connected to the provided node,
 	  and removes the provided node.*/
-	void deleteLeafAndBranches(Node* root);
+	void deleteLeafAndBranchesRecursive(Node* root);
+
+	/*adds the provided node to the binary tree at the suitable branch.
+	  recursive. Starting at the provided leaf pointer.*/
+	void insertNodeRecursive(Node* parentLeaf, Node* newNode);
+
+	/*recursively searches for a node which contains the provided value.
+	  Returns true if found, else false.*/
+	bool searchForValueRecursive(Node* root, int value) const;
+
+	/*recursively searches for a node which contains the provided value.
+	  if found, removes that node safely and returns true. else returns false.*/
+	bool searchAndDeleteLeafRecursive(Node* parentLeaf, int value);
+
+	void deleteLeaf(Node* )
 public:
 	/*initializes*/
 	BinaryTree(bool startWithRoot = false, int rootValue = 0);
 	~BinaryTree();
+
+	/*adds a node with the provided value to the binary tree*/
+	void addNode(int value);
+
+	/*Returns true if the binary tree contains a node with the provided value.
+	  Uses binary searching.*/
+	bool contains(int value) const;
+
+	/*returns true if node with value was found and removed, 
+	  else returns false.*/
+	bool remove(int value);
 };
